72bb7c8f1dc13fc60f84c9ec17a14c9b1f52d927
anshulg010 <kapil21102@iiitnr.edu.in>
2023-03-21 08:40:35 +0530

    ret

--- src/App.js
+++ src/App.js
@@ -1,57 +1,46 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Profile from './Profile'
-import Register from './Register'
-import VerifyEmail from './VerifyEmail';
-import Login from './Login'
-import {useState, useEffect} from 'react'
-import {AuthProvider} from './AuthContext'
-import {auth} from './firebase'
-import {onAuthStateChanged} from 'firebase/auth'
-import PrivateRoute from './PrivateRoute'
-import {Navigate} from 'react-router-dom'
-// import Background from './Rectangle/27.png'
 
+import React from "react"
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";import Dashboard from "./homepage/Dashboard"
+import Signup from "./homepage/SignUp"
+import { AuthProvider } from "./contexts/AuthContext"
+import Login from "./homepage/Login"
+import PrivateRoute from "./homepage/PrivateRoute"
+import ForgotPassword from "./homepage/ForgotPassword"
+import UpdateProfile from "./homepage/UpdateProfile"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Dashboard/>),
+  },
+  {
+    path: "/update-profile",
+    element: (<UpdateProfile/>),
+  },
+  {
+    path: "/signup",
+    element: (<Signup/>),
+  },
+  {
+    path: "/login",
+    element: (<Login/>),
+  },
+  {
+    path: "/forgot-password",
+    element: (<ForgotPassword/>),
+  },
+]);
 
 function App() {
-
-  const [currentUser, setCurrentUser] = useState(null)
-  const [timeActive, setTimeActive] = useState(false)
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user)
-    })
-  }, [])
-
-
   return (
-    // <div style={{ backgroundImage:`url(${Background})`,backgroundRepeat:"no-repeat",backgroundSize:"contain", width: 1200, height: 797}}>
-    <Router>
-      <AuthProvider value={{currentUser, timeActive, setTimeActive}}>
-        <Routes>
-          <Route exact path='/' element={
-            <PrivateRoute>
-              <Profile/>
-            </PrivateRoute>
-          }/>
-          <Route path="/login" element={
-            !currentUser?.emailVerified 
-            ? <Login/>
-            : <Navigate to='/' replace/>
-          } />
-          <Route path="/register" element={
-            !currentUser?.emailVerified 
-            ? <Register/>
-            : <Navigate to='/' replace/>
-          } />
-          <Route path='/verify-email' element={<VerifyEmail/>} /> 
-        </Routes>  
-      </AuthProvider>
-    </Router>
-    // </div>
-  );
+        <RouterProvider router={router} />
+  )
 }
 
-export default App;
+export default App
